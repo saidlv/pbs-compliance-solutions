9d082b9a7fa82744f86d94fab3c03ddb34cce0f1
ahsinali-17 <aajoker17@gmail.com>
2025-04-15 00:11:06 +0500

    Home,navbar & footer updated

--- src/home-page/Hero.jsx
+++ src/home-page/Hero.jsx
@@ -1,4 +1,4 @@
-"use client";
+ "use client";
 
 import CustomImage from "@/app/CustomImage";
 import CTA from "@/components/CTA";
@@ -25,38 +25,37 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="relative bg-brand-dark w-full h-screen overflow-hidden">
-      {/* Background Image */}
-      <motion.div
-        className="absolute inset-0 w-full h-full"
-        initial={{ opacity: 0, x: 0, scale: 0.75 }}
-        animate={{
-          opacity: 1,
-          x: isSmallScreen && isOpen ? -50 : 0,
-          scale: [0.75, 1.05, 1],
-        }}
-        transition={{
-          duration: initialRender ? 1.2 : 0.5,
-          ease: "easeOut",
-          delay: initialRender ? 0.5 : 0,
-        }}
-        aria-hidden="true"
-      >
-        <CustomImage
-          src="/pics/homebg.png"
-          alt="Background skyscrapers"
-          className="w-full h-full object-cover z-0"
-          layout="fill"
-          priority={true}
-          quality={100}
-        />
-      </motion.div>
+    <div className="relative bg-brand-dark">
+      <div className="min-h-screen relative flex flex-col lg:flex-row items-center justify-center lg:justify-between">
+        {/* Background Image - Optional */}
 
-      {/* Content Container */}
-      <div className="relative h-screen flex flex-col lg:flex-row items-center justify-between px-6 md:px-12 lg:px-24">
-        {/* Left Side - Image */}
+          <motion.div
+            className="absolute w-full inset-0 h-full"
+            initial={{ opacity: 0, x: 0, scale: 0.75 }}
+            animate={{
+              opacity: 1,
+              x: isSmallScreen && isOpen ? -50 : 0,
+              scale: [0.75, 1.05, 1],
+            }}
+            transition={{
+              duration: initialRender ? 1.2 : 0.5,
+              ease: "easeOut",
+              delay: initialRender ? 0.5 : 0,
+            }}
+            aria-hidden="true"
+          >
+            <CustomImage
+              src="/pics/homebg.png"
+              alt="Modern skyscrapers"
+              className="w-full h-full object-cover z-0"
+              layout="fill"
+              priority={true}
+              quality={100}
+            />
+          </motion.div>
+          {/* Left Side - Image */}
         <motion.div
-          className="w-full lg:w-1/3 h-[50vh] lg:h-[80vh] flex items-center justify-start relative z-20"
+          className="w-full lg:w-1/3 h-[500px] lg:h-[700px] flex items-end justify-start lg:justify-center relative z-20"
           initial={{ opacity: 0, y: 1000, scale: 0.95, x: 0 }}
           animate={{
             opacity: 1,
@@ -75,23 +74,22 @@ const Hero = () => {
             src="/pics/Building.png"
             alt="Modern skyscrapers"
             className="w-full h-full object-cover md:object-contain lg:object-cover object-top"
-            width={600}
-            height={800}
-            unoptimized={true}
+            width={700}
+            height={500}
             loading="lazy"
           />
         </motion.div>
 
         {/* Right Side - Text Content */}
         <motion.div
-          className="z-20 w-full lg:w-2/3 flex flex-col justify-center items-start text-left p-6 lg:p-10"
+          className="z-20 w-full lg:w-2/3 flex flex-col justify-center items-start text-left mt-10 p-6 lg:p-10"
           initial="hidden"
           animate="visible"
           transition={{ staggerChildren: 0.2 }}
         >
           {/* Main Heading */}
           <motion.h1
-            className="text-3xl md:text-4xl lg:text-5xl/[1.2] font-conthrax font-semibold text-brand-light mb-6 max-w-2xl"
+            className="text-xl lg:text-3xl font-conthrax font-semibold text-brand-light mb-6 max-w-2xl"
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{
@@ -103,9 +101,12 @@ const Hero = () => {
             Ensuring Compliance, Excellence and Peace of Mind in New York Construction
           </motion.h1>
 
+          <div className="w-[30%] h-1 bg-[#8AD5B7] mb-4 rounded-full"></div>
+
+
           {/* Subheading */}
           <motion.p
-            className="text-base md:text-lg font-poppins text-brand-light mb-4 max-w-xl"
+            className="text-base lg:text-lg font-poppins text-[#89A096] mb-6 max-w-xl"
             initial={{ opacity: 0, x: 100 }}
             animate={{
               opacity: [0, 0.25, 0.75, 1],
@@ -120,7 +121,7 @@ const Hero = () => {
           </motion.p>
 
           <motion.p
-            className="text-base md:text-lg font-poppins text-brand-light mb-6 max-w-xl"
+            className="text-base lg:text-lg font-poppins text-[#89A096] mb-2 max-w-xl"
             initial={{ opacity: 0, x: 100 }}
             animate={{
               opacity: [0, 0.25, 0.75, 1],
@@ -138,7 +139,7 @@ const Hero = () => {
           <CTA
             text="Schedule your Consulting Today"
             href="/contacts"
-            styling="rounded-full bg-brand-green2 text-black font-semibold font-poppins px-8 py-4 transition-colors hover:bg-brand-green1 max-w-full lg:max-w-md my-6"
+            styling="rounded-full bg-[#8AD5B7] text-black hover:from-brand-green1 hover:to-brand-green2 font-semibold font-conthrax px-8 py-4 transition-colors mt-0 max-w-full lg:max-w-xl my-6"
           />
         </motion.div>
       </div>
