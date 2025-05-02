53defd26130d366fe097cfe08a6be53193a69ed6
ahsinali-17 <aajoker17@gmail.com>
2025-05-02 23:14:52 +0500

    elevator-inspection

--- src/app/about-us/our-values/page.jsx
+++ src/app/about-us/our-values/page.jsx
@@ -33,7 +33,7 @@ const Page = () => {
   <div className="w-[100vw] relative flex justify-center">
     <div className="bg-[#8AD5B7] w-full h-2 sm:h-3 lg:h-4"></div>
     <div
-      className="bg-[#8AD5B7] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[80%] sm:w-[70%] lg:w-[60%] rounded-full text-[#37403D] p-2 sm:p-3 lg:p-4 flex justify-center items-center font-conthrax text-center text-base sm:text-xl md:text-2xl lg:text-3xl z-20"
+      className="bg-[#8AD5B7] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[60%] sm:w-[50%] lg:w-[40%] rounded-full text-[#37403D] p-2 sm:p-3 lg:p-4 flex justify-center items-center font-conthrax text-center text-base sm:text-xl md:text-2xl lg:text-3xl z-20"
     >
       Our Values
     </div>
