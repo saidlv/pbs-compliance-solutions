1478b05414a79518f78985b3d6d9a2f5f21aa1cb
ahsinali-17 <aajoker17@gmail.com>
2025-05-06 23:50:06 +0500

    navbar updated

--- src/home-page/Hero.jsx
+++ src/home-page/Hero.jsx
@@ -22,14 +22,14 @@ const HeroCarousel = () => {
     slidesToScroll: 1, 
     centerPadding: "0%", 
     arrows: false, 
-    autoplay: true, 
+    autoplay: false, 
     autoplaySpeed: 2000, 
   };
   return (
     <Slider {...settings} className="flex justify-evenly items-center">
       {heroSections.map((img, index) => (
          <div key={index} className="relative bg-brand-dark">
-         <div className="relative min-h-[90vh] flex items-center lg:items-end justify-center lg:justify-start p-8">
+         <div className="relative min-h-screen flex items-center lg:items-end justify-center lg:justify-start p-8">
            {/* Background Image - Optional */}
    
              <div
